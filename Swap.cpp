0e3ffcb25fd636acac49758d7d3e53b6c7fecfd7
khushboogarg41 <102350925+khushboogarg41@users.noreply.github.com>
2022-05-24 12:55:01 +0530

    Create Swap.cpp

---
+++ Swap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	float a,b,c;
+	cout<<"enter the value of a = ";
+	cin>>a;
+	cout<<"enter the value of  b = ";
+	cin>>b;
+	
+	a=b;
+	b=a;
+	cout<<"a = "<<a<<endl;
+	cout<<"b = "<<b;
+}
